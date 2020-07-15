3f1f39b32acd6beed59a37208cd73ecbdf48b682
Kent McKinney <kent.p.mckinney@gmail.com>
2020-07-14 22:47:29 -0700

    add missing data fields

--- app.py
+++ app.py
@@ -40,8 +40,15 @@ class Venue(db.Model):
     state = db.Column(db.String(120))
     address = db.Column(db.String(120))
     phone = db.Column(db.String(120))
+    genres = db.Column(db.String(500))
     image_link = db.Column(db.String(500))
+    website_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    upcoming_show_ids = db.Column(
+        db.Integer, db.ForeignKey('Show.id'), nullable=False)
+    past_show_ids = db.Column(
+        db.Integer, db.ForeignKey('Show.id'), nullable=False)
+    # shows = db.relationship('Show', backref='list', lazy=True)
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
@@ -54,15 +61,26 @@ class Artist(db.Model):
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
+    genre = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
+    website_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
-    # genre
-    # website_link
+    # shows = db.relationship('Show', backref='list', lazy=True)
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+
+
+class Show(db.Model):
+    __tablename__ = "Show"
+    id = db.Column(db.Integer, primary_key=True)
+    datetime = db.Column(db.DateTime)
+    artist_id = db.Column(db.Integer, db.ForeignKey(
+        'Artist.id'), nullable=False)
+    # image_link = db.Column(db.String(500), db.ForeignKey('Artist.image_link'), nullable=False)
+    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
+
 
 #----------------------------------------------------------------------------#
 # Filters.
